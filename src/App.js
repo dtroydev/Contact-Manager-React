52f6f2c899cd03c20612dbcfb9be9f5c882295cc
Daniel <daniel@Daniels-Air.home.gateway>
2018-09-26 21:07:46 +1000

    contacts, header, bootstrap

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from 'react';
+import Header from './components/Header';
+import Contact from './components/Contact';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-class App extends React.Component {
+export default class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1 className="App-title">Welcome to Contact Manager!</h1>
+        <Header branding="Contact Manager" />
+        <div className="container">
+          <Contact
+            name="John Doe"
+            email="jdoe@example.com"
+            phone="01 0010 0011"
+          />
+          <Contact
+            name="Karen Smith"
+            email="ksmith@example.com"
+            phone="01 0011 0100 "
+          />
+        </div>
       </div>
     );
   }
 }
-
-export default App;
