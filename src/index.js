2310b257e898f7f12f133ffddd3480037bd8ea4c
Daniel <daniel@Daniels-Air.home.gateway>
2018-10-17 17:02:45 +1100

    polyfill for older browser support

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 // ui library DOM attachment to root ui component
+import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
